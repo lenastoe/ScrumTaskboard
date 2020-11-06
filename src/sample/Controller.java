1aff294cb27b46c1b21afbcad2d01e0f762204f3
lenastoe <lenastoe@gmx.at>
2020-11-06 12:24:53 +0100

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class Controller{
+
+    @FXML
+    Button button1;
+
+    @FXML
+    public void initialize(){
+        System.out.println("lfaew4hhrqtqt");
+    }
+
+
+}
